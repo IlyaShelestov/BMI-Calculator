33b10c30c50aba674aeba91f5b9901e8349171c2
IlyaShelestov <shelestov_ilya2905@mail.ru>
2023-12-31 06:45:12 +0600

    Added POST method as route, installed body-parser package

--- routes/bmiRoute.js
+++ routes/bmiRoute.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const router = express.Router();
+
+router.use(bodyParser.urlencoded({extended: true}));
+router.use(bodyParser.json());
+
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'views', 'bmiCalculator.html'));
 });
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+})
 
 module.exports = router;
