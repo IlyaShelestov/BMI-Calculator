e3ae7da1267f165cb4dfca2229978a27b87dd2ec
IlyaShelestov <shelestov_ilya2905@mail.ru>
2023-12-31 06:05:16 +0600

    Added npm start command, added GET routes for home page and calculator page, root.js now can handle server

---
+++ root.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const bmiRoute = require('./routes/bmiRoute.js');
+const homeRoute = require('./routes/homeRoute.js');
+
+app.use(express.static('public'));
+
+app.use('/', homeRoute);
+app.use('/bmicalculator', bmiRoute);
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
