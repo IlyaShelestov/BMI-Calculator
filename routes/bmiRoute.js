ed022024c0cc4741ba932cd2416c5be552fa0c98
IlyaShelestov <shelestov_ilya2905@mail.ru>
2023-12-31 16:38:42 +0600

    Added calculator logic, info outputs to console, added female and male tables for teens and changed category's range

--- routes/bmiRoute.js
+++ routes/bmiRoute.js
@@ -13,6 +13,63 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
     console.log(req.body);
+    const weight = parseFloat(req.body.weight);
+    const height = parseFloat(req.body.height);
+    const age = parseFloat(req.body.age);
+    const gender = parseFloat(req.body.gender);
+    const units = parseFloat(req.body.units);
+    //CALCULATOR LOGIC
+    let bmi;
+    if (units === 0) { 
+        bmi = weight / Math.pow(height / 100, 2);
+    } else { 
+        bmi = (weight / Math.pow(height, 2)) * 703;
+    }
+    //ESTIMATE
+    let estimate = '-';
+    if (age > 20) {
+        if (bmi <= 16) {
+            estimate = 'Severe Thinness';
+        } else if (bmi <= 17) {
+            estimate = 'Moderate Thinness';
+        } else if (bmi <= 18.5) {
+            estimate = 'Mild Thinness';
+        } else if (bmi <= 25) {
+            estimate = 'Normal';
+        } else if (bmi <= 30) {
+            estimate = 'Overweight';
+        } else if (bmi <= 35) {
+            estimate = 'Obese Class I';
+        } else if (bmi <= 40) {
+            estimate = 'Obese Class II';
+        } else {
+            estimate = 'Obese Class III';
+        }
+    } else {
+        if (gender === 0) {
+            if (bmi <= 19) {
+                estimate = 'Underweight';
+            } else if (bmi <= 27) {
+                estimate = 'Healthy weight';
+            } else if (bmi <= 31) {
+                estimate = 'At risk of overweight';
+            } else {
+                estimate = 'Overweight';
+            }
+        } else {
+            if (bmi <= 17.8) {
+                estimate = 'Underweight';
+            } else if (bmi <= 26.5) {
+                estimate = 'Healthy weight';
+            } else if (bmi <= 32.5) {
+                estimate = 'At risk of overweight';
+            } else {
+                estimate = 'Overweight';
+            }
+        }
+    }
+    console.log('BMI: ' + bmi);
+    console.log('Estimate: ' + estimate);
 })
 
 module.exports = router;
